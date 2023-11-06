b01994d41192f3569a0d7a4901262a1dbf07b48d
baronovM <baronov05@gmail.com>
2023-11-07 02:17:32 +0400

    Docstrings

--- Caesar.py
+++ Caesar.py
@@ -2,8 +2,15 @@ import random
 
 
 class Caesar:
+    """
+    Класс, содержащий в виде статических методов функции, относящиеся к шифру Цезаря
+    """
     @staticmethod
     def handle_key(key: str) -> int:
+        """
+        Если ключ задан, функция переводит его из строки в число.
+        Иначе ключ выбирается случайным образом из диапазона [1. 255]
+        """
         if key:
             k = int(key)
         else:
@@ -12,19 +19,38 @@ class Caesar:
 
     @staticmethod
     def write_key(key: int):
+        """
+        Вывод ключа в консоль
+        """
         print(f"Your key: {key}")
 
     @staticmethod
     def encrypt(binary_sequence: bytearray, key: int):
+        """
+        Применяет к последовательности шифр Цезаря
+        :param binary_sequence: Шифруемая последовательность
+        :param key: Ключ шифрования
+        """
         for i in range(len(binary_sequence)):
             binary_sequence[i] = (binary_sequence[i] + key) % 256
 
     @staticmethod
     def decrypt(binary_sequence: bytearray, key: int):
+        """
+        Расшифровывает последовательность используя ключ
+        :param binary_sequence: Расшифровываемая последовательность
+        :param key: Ключ
+        """
         Caesar.encrypt(binary_sequence, -key)
 
     @staticmethod
     def crack(binary_sequence: bytearray) -> int:
+        """
+        Методом частотного анализа находит подходящий ключ, возвращает его
+        и расшифровывает последовательность
+        :param binary_sequence: Взламываемая последовательность
+        :return: Найденный ключ
+        """
         ans = 0
         best_result = 0
         with open("alphabet.txt") as alph:
--- Vernam.py
+++ Vernam.py
@@ -2,24 +2,48 @@ import random
 
 
 class Vernam:
+    """
+    Класс, содержащий в виде статических методов функции, относящиеся к шифру Вернама
+    """
     @staticmethod
     def handle_key(key_filename: str) -> str:
+        """
+        Если имя файла для хранения ключа задано, возвращает его же.
+        Иначе возвращает "key", так как по умолчанию ключ хранится в файле "key"
+        :param key_filename: Файл для хранения ключа (может быть None)
+        :return: Файл для хранения ключа (если был передан None, то вернётся "key")
+        """
         if key_filename:
             return key_filename
         return "key"
 
     @staticmethod
     def write_key(key_filename: str):
+        """
+        Записывает ключ шифрования в файл
+        :param key_filename: Имя файла для записи
+        """
         with open(key_filename, "wb") as fkey:
             fkey.write(Vernam.key)
 
     @staticmethod
     def encrypt(binary_sequence: bytearray, key_filename: str):
+        """
+        Применяет к последовательности шифр Вернама
+        :param binary_sequence: Шифруемая последовательность
+        :param key_filename: Файл для записи ключа (в фуекции этот аргумент не используется,
+        принимается чтобы ф-ия имела ту же сигнатуру, что и функции шифрования у других шифров)
+        """
         Vernam.key = bytearray(random.randbytes(len(binary_sequence)))
         binary_sequence[:] = [binary_sequence[i] ^ Vernam.key[i] for i in range(len(binary_sequence))]
 
     @staticmethod
     def decrypt(binary_sequence: bytearray, key_filename: str):
+        """
+        Расшифровывает зашифрованную шифром Вернама последовательность используя ключ из файла с переданным названием
+        :param binary_sequence: Расшифровываемая последовательность
+        :param key_filename: Имя файла с ключом шифрования
+        """
         with open(key_filename, "rb") as fkey:
             key = bytearray(fkey.read())
         binary_sequence[:] = [binary_sequence[i] ^ key[i] for i in range(len(binary_sequence))]
